46ad6b59e595577005068e0307416ba22b3a91bf
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2023-05-01 11:26:51 -0400

    release candidate 0.6
    seems to be nearly complete, no bug checking begun yet

--- extension/ERC1155BrandManagement.sol
+++ extension/ERC1155BrandManagement.sol
@@ -1,38 +1,101 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.18;
 
-import "./ERC1155Subletting.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-abstract contract ERC1155BrandManagement is ERC1155Subletting{
+contract BrandingUpgradeable is Initializable, AccessControlUpgradeable {
+    // must be declared
+    uint128 brandFee;
+
+    bytes16 BRAND_ADMIN = bytes16(keccak256("BRAND_ADMIN"));
+    bytes16 BRAND_MANAGER = bytes16(keccak256("BRAND_MANAGER"));
+
     uint128 nextBrandId = 0;
+    struct brand  {
+        string name;
+        string uri;
+        uint128[] tokenIds;
+    }
+    mapping(uint128 => brand) private brandMap;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    // helper function for calculating (concatenating) roles
+    function figureRole(uint128 _brandId, bytes16 _halfHashedRole) internal pure returns (bytes32) {
+        bytes memory packed = abi.encodePacked(bytes16(_brandId), _halfHashedRole);
+        bytes32 result;
+        assembly { result := mload(add(packed, 32)) }
+        return result;
+    }
+
+    // restricts Access to Administrators of a Brand
+    modifier onlyBrandAdmin(uint128 _brandId) {
+        _checkRole(getBRAND_ADMIN(_brandId));
+        _;
+    }
+
+    // restricts Access to Managers of a Brand
+    modifier onlyBrandManager(uint128 _brandId) {
+        _checkRole(figureRole(_brandId, BRAND_MANAGER));
+        _;
+    }
+
+
+    function createBrand(string memory _name, string memory _uri) public payable {
+        require(msg.value >= brandFee, "Insufficiant payment");
+        require(bytes(_name ).length != 0);
+        brandMap[nextBrandId].name = _name;
+        brandMap[nextBrandId].uri = _uri;
+        _setupRole(figureRole(nextBrandId, bytes16(keccak256("BRAND_ADMIN"))), msg.sender);
+        _setupRole(figureRole(nextBrandId, bytes16(keccak256("BRAND_MANAGER"))), msg.sender);
+        nextBrandId ++;
+        // MUST trigger event
+    }
+
+    function updateBranding(uint128 _brandId, string memory _name, string memory _uri) public onlyBrandManager(_brandId) {
+        brandMap[_brandId].name = _name;
+        brandMap[_brandId].uri = _uri;
+        // MUST trigger event
+    }
+
+    function readBranding(uint128 _brandId) public view returns (string memory, string memory) {
+        return (brandMap[_brandId].name,
+        brandMap[_brandId].uri);
+    }
+
+    function initialize() public virtual initializer {
+        __AccessControl_init();
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(figureRole(0, BRAND_MANAGER), msg.sender);
+        createBrand("Dyvvy Nation", "https://dyvvy.org");
+    }
+
+    function getBRAND_ADMIN(uint128 _brandId) public view returns (bytes32) {
+        return figureRole(_brandId, BRAND_ADMIN);
+    }
+
+    function getBRAND_MANAGER(uint128 _brandId) public view returns (bytes32) {
+        return figureRole(_brandId, BRAND_MANAGER);
+    }
+
+    function setBRAND_MANAGER(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) {
+        _setupRole(figureRole(_brandId, BRAND_MANAGER), _assignee);
+    } 
     
-    struct brandData {
-        string brandName;
-        address brandAdmin;
-        string brandUri;
-        uint128[] brandedTokenIds;
+    function setBRAND_ADMIN(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) {
+        _setupRole(figureRole(_brandId, BRAND_ADMIN), _assignee);
+    } 
+
+    function revokeManagerRole(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) {
+        _revokeRole(figureRole(_brandId, BRAND_MANAGER), _assignee);
     }
 
-    mapping(uint128 => brandData) brandDataMap;
-
-    // expected needed modifiers
-    modifier onlyBrandAdmin(uint128 brandId) {
-        require(brandDataMap[brandId].brandAdmin == msg.sender, "Brand Administrator privelage required.");
-        _;}
-    modifier onlyBrandAdminOfToken(uint256 tokenId) {
-        // require(token(exists));
-        // getTokenBrand;
-        // check Brand Admin against msg.sender;
-        _;}
-
-    function createBrand(string memory _brand, string memory _brandUri) public payable {
-        // Set the next Brand's state
-        brandDataMap[nextBrandId].brandName = _brand;
-        brandDataMap[nextBrandId].brandAdmin = msg.sender;
-        brandDataMap[nextBrandId].brandUri = _brandUri;
-        // iterate counter
-        nextBrandId++;
+    function transferAndRevokeBrandAdmin(uint128 _brandId, address _newOwner) public payable onlyBrandAdmin(_brandId) {
+        _setupRole(getBRAND_ADMIN(_brandId), _newOwner);
+        renounceRole(getBRAND_ADMIN(_brandId), msg.sender);
+        // MUST trigger event (i think is does with renouncRole()
     }
-
-
 }
