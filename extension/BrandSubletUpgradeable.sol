c57713c361cca6a48ec6d77bc880d8d58b5aae12
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2023-05-12 10:11:27 -0400

    Update BrandSubletUpgradeable.sol

--- extension/BrandSubletUpgradeable.sol
+++ extension/BrandSubletUpgradeable.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 // add events and comments
 import "./SubletControlUpgradeable.sol";
 
-contract BrandingSubletUpgradeable is SubletControlUpgradeable {
+abstract contract BrandingSubletUpgradeable is SubletControlUpgradeable {
     // must be declared
     uint128 brandFee;
 
@@ -13,7 +13,7 @@ contract BrandingSubletUpgradeable is SubletControlUpgradeable {
     struct brand  {
         string name;
         string uri;
-        uint128[] tokenIds;
+        uint128[] uriIds;
     }
     mapping(uint128 => brand) private brandMap;
 
@@ -30,7 +30,7 @@ contract BrandingSubletUpgradeable is SubletControlUpgradeable {
     }
 
 
-    function createBrand(string memory _name, string memory _uri) public payable {
+    function createBrand(string memory _name, string memory _uri) public payable returns (uint128) {
         require(msg.value >= brandFee, "Insufficiant payment");
         require(bytes(_name ).length != 0);
         brandMap[nextBrandId].name = _name;
@@ -38,34 +38,78 @@ contract BrandingSubletUpgradeable is SubletControlUpgradeable {
         _setupRole(getRole(nextBrandId, "BRAND_ADMIN"), msg.sender);
         _setupRole(getRole(nextBrandId, "BRAND_MANAGER"), msg.sender);
         nextBrandId ++;
+        return nextBrandId - 1;
         // MUST trigger event
     }
 
-    function updateBranding(uint128 _brandId, string memory _name, string memory _uri) public onlyBrandManager(_brandId) {
+    function updateBranding(uint128 _brandId, string memory _name, string memory _uri) public onlyBrandManager(_brandId) returns (bool) {
         brandMap[_brandId].name = _name;
         brandMap[_brandId].uri = _uri;
         // MUST?SHOULD? trigger event
+        return true;
     }
 
-    function readBranding(uint128 _brandId) public view returns (string memory, string memory URI, uint128[] memory Tokens, bytes32 ADMIN_ROLE, bytes32 MANAGER_ROLE) {
+    function readBranding(uint128 _brandId) public view returns (string memory, string memory URI, uint128[] memory Tokens, bytes32 BRAND_ADMIN, bytes32 BRAND_MANAGER) {
         return (brandMap[_brandId].name,
         brandMap[_brandId].uri,
-        brandMap[_brandId].tokenIds,
+        brandMap[_brandId].uriIds,
         getRole(_brandId, "BRAND_ADMIN"),
         getRole(_brandId, "BRAND_MANAGER"));
     }
 
-    function setBRAND_MANAGER(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) {
+    function setBRAND_MANAGER(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) returns (bool) {
         _setupRole(getRole(_brandId, "BRAND_MANAGER"), _assignee);
+        return true;
     } 
     
-    function setBRAND_ADMIN(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) {
+    function setBRAND_ADMIN(uint128 _brandId, address _assignee) public onlyBrandAdmin(_brandId) returns (bool) {
         _setupRole(getRole(_brandId, "BRAND_ADMIN"), _assignee);
+        return true;
     } 
 
-    function transferAndRenounceBrandAdmin(uint128 _brandId, address _newOwner) public onlyBrandAdmin(_brandId) {
+    function transferAndRenounceBrandAdmin(uint128 _brandId, address _newOwner) public onlyBrandAdmin(_brandId) returns (bool) {
         _setupRole(getRole(_brandId, "BRAND_ADMIN"), _newOwner);
         renounceRole(getRole(_brandId, "BRAND_ADMIN"), msg.sender);
-        // MUST trigger event (i think is does with renouncRole()
+        return true;
+        // MUST trigger event (ACU renounceRole() might do this already)
+    }
+
+    // Brands may claim batches of uriIds which share a uri, saving storage and gas fees to store once per batch uri
+
+    uint128 _nextUriId;
+    struct uriHelper {
+        uint128 upperLimit;
+        string uri;
+    }
+    mapping(uint128 => uriHelper) internal uriMap;
+
+    // Functions
+    function batchURI(uint128 _uriId) public returns (string memory) {
+        require(_exists(_uriId) == true, "Nonexistent token");
+        return bytes(_baseURI(_uriId)).length != 0 ? string(abi.encodePacked(_baseURI(_uriId), "/", _uriId)) : '';
+    }
+
+    function _baseURI(uint128 _uriId) internal view returns (string memory) {
+        uint128 lowerLimit = 0;
+        while (lowerLimit < _nextUriId) {
+            if(lowerLimit <= _uriId && _uriId < uriMap[lowerLimit].upperLimit) break;
+            lowerLimit = uriMap[lowerLimit].upperLimit;
+        }
+        return uriMap[lowerLimit].uri;
+    }
+
+    // MUST only update existing entries
+    function updateBatchofURIs(uint128 _lowerLimit, string memory _newURI) public onlyRole(getRole(_lowerLimit, "BRAND_MANAGER")) {
+        if(bytes(uriMap[_lowerLimit].uri).length !=0) uriMap[_lowerLimit].uri = _newURI;
+    }
+
+    // MUST only update ONE 'tuple'
+    function allocateAndSetURIs(uint128 quantity, string memory uri) public onlyRole(getRole(_nextUriId, "BRAND_MANAGER")) virtual {
+        require(quantity>0);
+        uriMap[_nextUriId] = uriHelper(_nextUriId + quantity,uri);
+    }
+
+    function _exists(uint128 _uriId) internal returns (bool) {
+
     }
 }
