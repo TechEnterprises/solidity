636597ae77ea1213b8becd4e9e655d788b861c7e
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2023-07-17 08:10:52 -0400

    Update My1155x20Token.sol

--- base/My1155x20Token.sol
+++ base/My1155x20Token.sol
@@ -1,1 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "../extension/ERC1155x20.sol";
+
+contract My1155Token is Ownable, Pausable, ERC1155Burnable, ERC1155Supply, ERC20Extended {
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply, string memory _uri, bytes memory _data) 
+        ERC1155(_uri) {
+        name = _name;
+        symbol = _symbol;
+        _mint(msg.sender, 0, _initialSupply, _data);
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        whenNotPaused
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+}
 
