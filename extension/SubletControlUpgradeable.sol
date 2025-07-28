cf856fb634b7c6d8ec7741d9191c0e0a1f92d6b1
Jeffrey Anthony <jeffanthonyfds@gmail.com>
2025-07-28 17:04:55 -0400

    Update SubletControlUpgradeable.sol
    add comment note

--- extension/SubletControlUpgradeable.sol
+++ extension/SubletControlUpgradeable.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
+// Provides helper utilities for creating per‑token roles by combining a token ID and a role string:
+// function getRole(uint128 _brandId, string memory _role) internal pure returns (bytes32) {
+//    bytes memory packed = abi.encodePacked(bytes16(_brandId), stringToBytes16(_role));
+//    bytes32 result;
+//    assembly { result := mload(add(packed, 32)) }
+//    return result;
+// }
+ 
 abstract contract SubletControlUpgradeable is Initializable, AccessControlUpgradeable {
 
     // intended to allow the bytes32 _role of ACU to integrate the uint128 tokenId of a 721, 721A or 1155 contract
